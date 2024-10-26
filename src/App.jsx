c7c0b61803e3e218514d05e916335b13cb529e4d
CodeEaseWithAnu <ganumols@gmail.com>
2024-10-26 18:18:47 +0530

    'infinite'

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InfiniteScroll from './component/InfiniteScroll'
+
+const App = () => {
+  return (
+    <div>
+      <InfiniteScroll></InfiniteScroll>
+    </div>
+  )
+}
+
+export default App
